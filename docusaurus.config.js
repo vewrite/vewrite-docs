db8a10fca17ecce836f1821d9649be17ffc44653
Rami James <ramijames@gmail.com>
2025-02-25 14:17:27 +0200

    Trying to fix netlify build failure

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
   organizationName: 'vewrite', // Usually your GitHub org/user name.
   projectName: 'vewrite-docs', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
